4b7304830c58044ab9d9eb60e0eec58ed51d765b
Nvmeless <alexandre.quilan.delaistre@gmail.com>
2024-09-26 12:01:05 +0200

    Crud Operatioonnel

--- src/Controller/ChimpokomonController.php
+++ src/Controller/ChimpokomonController.php
@@ -3,91 +3,131 @@
 namespace App\Controller;
 
 use App\Entity\Chimpokomon;
+use App\Repository\ChimpokodexRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ChimpokomonRepository;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ChimpokomonController extends AbstractController
 {
-    #[Route('/chimpokomon', name: 'app_chimpokomon_getAll')]
+    #[Route('/chimpokomon', name: 'app_chimpokomon_getAll', methods: ['GET'])]
     public function getAllChimpokomons(
         ChimpokomonRepository $chimpokomonRepository,
-        SerializerInterface $serializer
-    ): JsonResponse
-    {
-        $jsonChimpokos = $serializer->serialize($chimpokomonRepository->findStatusOn(), 'json',["groups" => "chimpokomon"]);
-        return new JsonResponse($jsonChimpokos, Response::HTTP_OK, [], true);
+        SerializerInterface $serializer,
+        TagAwareCacheInterface $cache
+    ): JsonResponse {
+        $idCache = "getAllChimpokomons";
+        $cachedChimpokos = $cache->get($idCache, function (ItemInterface $item) use ($chimpokomonRepository, $serializer) {
+            $item->tag("chimpokomonCache");
+            echo "Mise en cache";
+            $chimpokolist = $chimpokomonRepository->findStatusOn();
+            $jsonChimpokos = $serializer->serialize($chimpokolist, 'json', ['groups' => "chimpokomon"]);
+            return $jsonChimpokos;
+        });
+
+
+
+
+        // $jsonChimpokos = $serializer->serialize($chimpokomonRepository->findStatusOn(), 'json', ["groups" => "chimpokomon"]);
+        return new JsonResponse($cachedChimpokos, Response::HTTP_OK, [], true);
         // return $this->json($jsonChimpokos);
     }
 
 
-    #[Route('/chimpokomon/{chimpokomon}', name: 'app_chimpokomon_get', methods:['GET'])]
+    #[Route('/chimpokomon/{chimpokomon}', name: 'app_chimpokomon_get', methods: ['GET'])]
     public function getChimpokomon(
         Chimpokomon $chimpokomon
-    ): JsonResponse
-    {
+    ): JsonResponse {
         return $this->json($chimpokomon);
     }
 
 
 
-    #[Route('/chimpokomon', name:'app_chimpokon_create', methods:["POST"])]
-      public function createChimpokomon(
+    #[Route('/chimpokomon', name: 'app_chimpokon_create', methods: ["POST"])]
+    public function createChimpokomon(
         Request $request,
-        EntityManagerInterface $entityManager
-    ): JsonResponse
-    {
+        EntityManagerInterface $entityManager,
+        TagAwareCacheInterface $cache,
+        UrlGeneratorInterface $urlGenerator,
+        ChimpokodexRepository $chimpokodexRepository,
+        SerializerInterface $serializer
+    ): JsonResponse {
         $requestData = $request->toArray();
-        $newChimpo = new Chimpokomon();
-        $newChimpo->setName($requestData["name"]);
+        $newChimpo = $serializer->deserialize($request->getContent(), Chimpokomon::class, "json");
         $newChimpo->setStatus('on');
+        $chimpokodex = $chimpokodexRepository->find($requestData["chimpokodexId"]);
+        $newChimpo->setChimpokodex($chimpokodex ?? null);
+        if ($requestData["pvMax"] > $chimpokodex->getPvMax()) {
+            $newChimpo->setPvMax($chimpokodex->getPvMax());
+        }
         $entityManager->persist($newChimpo);
         $entityManager->flush();
-        return $this->json($newChimpo);
+        $cache->invalidateTags(['chimpokomonCache']);
+        $jsonChimpokomon = $serializer->serialize($newChimpo, 'json', ['groups' => 'chimpokomon']);
+
+        $location = $urlGenerator->generate('app_chimpokomon_get', ['chimpokomon' => $newChimpo->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonChimpokomon, JsonResponse::HTTP_CREATED, ["Location" => $location], true);
+        // return $this->json();
     }
 
-    #[Route('/chimpokomon/{chimpokomon}', name: 'app_chimpokomon_update', methods:['PUT', 'PATCH'])]
-      public function updateChimpokomon(
+    #[Route('/chimpokomon/{chimpokomon}', name: 'app_chimpokomon_update', methods: ['PUT', 'PATCH'])]
+    public function updateChimpokomon(
         Chimpokomon $chimpokomon,
         Request $request,
-        EntityManagerInterface $entityManager
-    ): JsonResponse
-    {
+        EntityManagerInterface $entityManager,
+        TagAwareCacheInterface $cache,
+        SerializerInterface $serializer,
+    ): JsonResponse {
 
-
-        $chimpokomon->setName($request->toArray()['name'] ?? $chimpokomon->getName());
-        $entityManager->persist($chimpokomon);
+        $requestData = $request->toArray();
+        $newChimpo = $serializer->deserialize($request->getContent(), Chimpokomon::class, "json", [AbstractNormalizer::OBJECT_TO_POPULATE => $chimpokomon]);
+        $newChimpo->setStatus('on');
+        $chimpokodex = $newChimpo->getChimpokodex();
+        if ($requestData["pvMax"] > $chimpokodex->getPvMax()) {
+            $newChimpo->setPvMax($chimpokodex->getPvMax());
+        }
+        $entityManager->persist($newChimpo);
         $entityManager->flush();
-        return $this->json($chimpokomon);
+        $cache->invalidateTags(['chimpokomonCache']);
+
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+
     }
 
 
 
 
-    #[Route('/chimpokomon/{chimpokomon}', name: 'app_chimpokomon_delete', methods:['DELETE'])]
+    #[Route('/chimpokomon/{chimpokomon}', name: 'app_chimpokomon_delete', methods: ['DELETE'])]
     public function deleteChimpokomon(
         Chimpokomon $chimpokomon,
         EntityManagerInterface $entityManager,
-        Request $request
-    ): JsonResponse
-    {
+        Request $request,
+        TagAwareCacheInterface $cache
+    ): JsonResponse {
         $force = $request->toArray()["force"] ?? false;
-        if($force){
+        if ($force) {
             $entityManager->remove($chimpokomon);
 
-        }else{
+        } else {
             $chimpokomon->setStatus('off');
             $entityManager->persist($chimpokomon);
         }
-
         $entityManager->flush();
+        $cache->invalidateTags(['chimpokomonCache']);
 
-        return $this->json(null);
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+
     }
 
 }
--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -14,44 +14,38 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 class MediaController extends AbstractController
 {
 
-    #[Route('/', name:'app_media')]
-    public function index():JsonResponse
+    #[Route('/', name: 'app_media')]
+    public function index(): JsonResponse
     {
-             return $this->json([]);
+        return $this->json([]);
     }
 
-    #[Route('/media/{media}', name: 'app_media_get', methods:['GET'])]
-    public function getMedia( 
+    #[Route('/media/{media}', name: 'app_media_get', methods: ['GET'])]
+    public function getMedia(
         Media $media,
         SerializerInterface $serializer,
         UrlGeneratorInterface $urlGenerator
-    )
-    {
-       $publicPath = $media->getPublicPath();
+    ) {
+        $publicPath = $media->getPublicPath();
         $location = $urlGenerator->generate("app_media", [], UrlGeneratorInterface::ABSOLUTE_URL);
-        $location .= $publicPath . "/".$media->getRealPath();        
+        $location .= $publicPath . "/" . $media->getRealPath();
         $jsonMedia = $serializer->serialize($media, 'json');
-        return $media ? 
-        new JsonResponse($jsonMedia, JsonResponse::HTTP_OK, [
-            "Location" => $location
-        ], true):
-        new JsonResponse(null, JsonResponse::HTTP_NOT_FOUND);
+        return $media ?
+            new JsonResponse($jsonMedia, JsonResponse::HTTP_OK, [
+                "Location" => $location
+            ], true) :
+            new JsonResponse(null, JsonResponse::HTTP_NOT_FOUND);
 
     }
 
 
-
-
-
-
-
-    #[Route('/media', name: 'app_media_create', methods:['POST'])]
+    #[Route('/media', name: 'app_media_create', methods: ['POST'])]
     public function createMedia(
-        Request $request, 
-        EntityManagerInterface $entityManager, 
-        SerializerInterface $serializer
-    ): JsonResponse
-    {
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer,
+        UrlGeneratorInterface $urlGenerator
+    ): JsonResponse {
 
         $media = new Media();
         $file = $request->files->get('media');
@@ -63,9 +57,9 @@ class MediaController extends AbstractController
         $entityManager->flush();
 
 
-        $jsonFile = $serializer->serialize($media,'json');
+        $jsonFile = $serializer->serialize($media, 'json');
 
-
-        return new JsonResponse($jsonFile, JsonResponse::HTTP_CREATED, [], true);
+        $location = $urlGenerator->generate('app_media_get', ['media' => $media->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse($jsonFile, JsonResponse::HTTP_CREATED, ['Location' => $location], true);
     }
 }
